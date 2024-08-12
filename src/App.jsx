86678ed6195d4d3cb21d19411a87ebd69fb82c1d
hro19 <fourbasic19@gmail.com>
2024-08-12 19:05:37 +0900

    chore: Add Button component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './stories/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,16 +19,20 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        count is {count}
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button 
+        primary // primary なボタン
+        size="large" // サイズを large に
+        backgroundColor="#26c6da" // 背景色を指定
+        label="Click me!" 
+        onClick={() => setCount((count) => count + 1)} 
+      />
     </div>
   )
 }
